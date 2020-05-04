3c325059882c7f7a63b09a3a855138ed8443dd26
Holger H <holgheiss@gmail.com>
2020-05-04 17:10:41 +0200

    hack simple perspective distortion thanks to https://github.com/metafizzy/zdog/issues/2

--- js/canvas-renderer.js
+++ js/canvas-renderer.js
@@ -13,6 +13,14 @@
   }
 }( this, function factory() {
 
+let fov = window && window.fov;
+if (fov) {
+  function scale(z) {return fov/(fov+z)};
+} else {
+  function scale(z) {return 1};
+}
+
+
 var CanvasRenderer = { isCanvas: true };
 
 CanvasRenderer.begin = function( ctx ) {
@@ -20,15 +28,20 @@ CanvasRenderer.begin = function( ctx ) {
 };
 
 CanvasRenderer.move = function( ctx, elem, point ) {
-  ctx.moveTo( point.x, point.y );
+  let s = scale(point.z);
+  ctx.moveTo( s*point.x, s*point.y );
 };
 
 CanvasRenderer.line = function( ctx, elem, point ) {
-  ctx.lineTo( point.x, point.y );
+  let s = scale(point.z);
+  ctx.lineTo( point.x * s, point.y * s);
 };
 
 CanvasRenderer.bezier = function( ctx, elem, cp0, cp1, end ) {
-  ctx.bezierCurveTo( cp0.x, cp0.y, cp1.x, cp1.y, end.x, end.y );
+  let s0 = scale(cp0.z);
+  let s1 = scale(cp1.z);
+  let se = scale(end.z);
+  ctx.bezierCurveTo( s0 * cp0.x, s0 * cp0.y, s1*cp1.x, s1*cp1.y, se*end.x, se*end.y );
 };
 
 CanvasRenderer.closePath = function( ctx ) {
@@ -41,7 +54,7 @@ CanvasRenderer.renderPath = function( ctx, elem, pathCommands, isClosed ) {
   this.begin( ctx, elem );
   pathCommands.forEach( function( command ) {
     command.render( ctx, elem, CanvasRenderer );
-  } );
+  });
   if ( isClosed ) {
     this.closePath( ctx, elem );
   }
@@ -68,4 +81,4 @@ CanvasRenderer.end = function() {};
 
 return CanvasRenderer;
 
-} ) );
+}));
